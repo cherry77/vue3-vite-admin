6e51c6cd0f65b63bd5faa958b063bd13b9a54a8c
flora <15071285862@163.com>
2021-11-14 20:28:08 +0800

    something change

--- src/layout/default/menu/index.vue
+++ src/layout/default/menu/index.vue
@@ -1,12 +1,16 @@
-<!-- <MenuItem v-for="item in items" :key="item.path">
+<template>
+<!-- https://cloud.tencent.com/developer/article/1718737/ -->
+  <AppLogo :class="getLogoClass"/>
+  <Menu
+    mode="inline"
+    theme="dark"
+    :inline-collapsed="state.collapsed"
+    v-model:openKeys="state.openKeys"
+    v-model:selectedKeys="state.selectedKeys"
+  >
+    <MenuItem key="1">
       <template #icon>
         <PieChartOutlined />
-      </template>
-      <span>{{t('routes.dashboard.dashboard')}}</span>
-    </MenuItem> -->
-<!-- <MenuItem key="1">
-      <template #icon>
-        <DesktopOutlined />
       </template>
       <span>Option 1</span>
     </MenuItem>
@@ -21,8 +25,8 @@
         <InboxOutlined />
       </template>
       <span>Option 3</span>
-    </MenuItem> -->
-<!-- <SubMenu key="sub1">
+    </MenuItem>
+    <SubMenu key="sub1">
       <template #icon>
         <MailOutlined />
       </template>
@@ -45,15 +49,12 @@
       </SubMenu>
     </SubMenu>
   </Menu>
-</template> -->
-<script lang="tsx">
-import { reactive, computed, defineComponent } from "vue";
-import { Menu, MenuItem, SubMenu } from "ant-design-vue";
-import { AppLogo } from "@/components/Application";
-import { useDesign } from "@/hooks/web/useDesign";
-import items from "@/mock/sys/menu";
-import { useI18n } from "vue-i18n";
-
+</template>
+<script lang="ts" setup>
+import { reactive, computed } from 'vue'
+import { useDesign } from '@/hooks/web/useDesign'
+import { Menu, MenuItem, SubMenu } from 'ant-design-vue'
+import { AppLogo } from '@/components/Application';
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
@@ -62,102 +63,47 @@ import {
   DesktopOutlined,
   InboxOutlined,
   AppstoreOutlined,
-} from "@ant-design/icons-vue";
-export default defineComponent({
-  setup(props, context) {
-    function generateMenu(menus) {
-      let vdom = [];
+} from '@ant-design/icons-vue';
 
-      if (menus instanceof Array) {
-        let list = [];
-        for (var item of menus) {
-          list.push(generateMenu(item));
-        }
-        vdom.push(<ul key="single">{list}</ul>);
-      } else {
-        vdom.push(
-          <li key={menus.menuId}>
-            <h1>{menus.name}</h1>
-            {generateMenu(menus.children)}
-          </li>
-        );
-      }
-      return vdom;
-      // return menus.map(item => {
-      //   if(item.children && item.children.length){
-      //     return (
-      //       <SubMenu key={item.path}>
-      //         {{
-      //           icon: () => <AppstoreOutlined />,
-      //           title: () => 'Navigation Two'
-      //         }}
-      //         <MenuItem key="9">Option 9</MenuItem>
-      //         <MenuItem key="10">Option 10</MenuItem>
-      //         <SubMenu key="sub3" title="Submenu">
-      //           <MenuItem key="11">Option 11</MenuItem>
-      //           <MenuItem key="12">Option 12</MenuItem>
-      //         </SubMenu>
-      //       </SubMenu>
-      //     )
-      //   }else{
-      //     return (
-      //       <MenuItem key="1">
-      //         <span>Option 1</span>
-      //       </MenuItem>
-      //     )
-      //   }
-      // });
-    }
-
-    return () => {
-      return generateMenu(items);
-    };
-  },
+const state = reactive({
+  collapsed: false,
+  selectedKeys: ['1'],
+  openKeys: ['sub1'],
+  preOpenKeys: ['sub1'],
 });
 
-console.log(items);
-
-// const { t } = useI18n()
-// const state = reactive({
-//   collapsed: false,
-//   selectedKeys: ['1'],
-//   openKeys: ['sub1'],
-//   preOpenKeys: ['sub1'],
-// });
-
-// const { prefixCls } = useDesign('layout-menu');
-
-// const getLogoClass = computed(() => {
-//   return [
-//     `${prefixCls}-logo`,
-//     // unref(getComputedMenuTheme),
-//     // {
-//     //   [`${prefixCls}--mobile`]: unref(getIsMobile),
-//     // },
-//   ];
-// });
+const { prefixCls } = useDesign('layout-menu');
+const getLogoClass = computed(() => {
+  return [
+    `${prefixCls}-logo`,
+    // unref(getComputedMenuTheme),
+    // {
+    //   [`${prefixCls}--mobile`]: unref(getIsMobile),
+    // },
+  ];
+});
 </script>
 <style lang="less">
-@prefix-cls: ~"@{namespace}-layout-menu";
-@logo-prefix-cls: ~"@{namespace}-app-logo";
+  @prefix-cls: ~'@{namespace}-layout-menu';
+  @logo-prefix-cls: ~'@{namespace}-app-logo';
 
-.@{prefix-cls} {
-  &-logo {
-    height: @header-height;
-    padding: 10px 4px 10px 10px;
+  .@{prefix-cls} {
+    &-logo {
+      height: @header-height;
+      padding: 10px 4px 10px 10px;
 
-    // img {
-    //   width: @logo-width;
-    //   height: @logo-width;
-    // }
-  }
+      // img {
+      //   width: @logo-width;
+      //   height: @logo-width;
+      // }
+    }
 
-  &--mobile {
-    .@{logo-prefix-cls} {
-      &__title {
-        opacity: 100%;
+    &--mobile {
+      .@{logo-prefix-cls} {
+        &__title {
+          opacity: 100%;
+        }
       }
     }
   }
-}
 </style>
