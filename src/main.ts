29c8eabdadb38f7dfd47077ba6d4e1ab3cec2d0b
flora <15071285862@163.com>
2022-01-12 22:08:48 +0800

    remove lowercode

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { setupRouterGuard } from '@/router/guard'
 import { setupStore } from '@/store';
 import { setupI18n } from '@/locale/setupI18n'
 // import { setupAntdIcon } from '@/components/AntIcon'
-import { setupCustomeComponent } from '@/views/lowercode/custom-components'
+
 
 import '@/design/index.less';
 // if (import.meta.env.DEV) {
@@ -31,8 +31,6 @@ async function bootstrap() {
 
   // icon
   // setupAntdIcon(app)
-
-  setupCustomeComponent(app)
 
   app.mount('#app')
 }
